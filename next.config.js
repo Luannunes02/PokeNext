5d2e1ff720cd9f9dbcc5c38c07bf8a37985183b6
Luannunes02 <nunesesbaltar.luan02@gmail.com>
2022-10-30 18:18:16 -0300

    Release

--- next.config.js
+++ next.config.js
@@ -1,23 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['cdn.traction.one'],    
-  },
   swcMinify: true,
-  
+
 }
 
 module.exports = nextConfig
-module.exports = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.traction.one',
-        port: '',
-        pathname: 'pokedex/pokemon/',
-      },
-    ],
-  },
-}
